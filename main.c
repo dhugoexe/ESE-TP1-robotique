4e4070be42ee636525b4dad46fd576f9b0a19144
dhugoexe <hugodevaux@gmail.com>
2024-09-10 14:21:55 +0200

    modif main

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+#define TAB_SIZE 10
+
 int main(void) {
+
+
+    int tab[TAB_SIZE];
+
+    for (int i=0;i<TAB_SIZE; i++) {
+        tab[i] = i;
+    }
     printf("Hello, World!\n");
     return 0;
 }
