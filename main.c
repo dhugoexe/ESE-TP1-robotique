b21eb2ff3ec7ab8b4abd40290c06a7f020928cbb
dhugoexe <hugodevaux@gmail.com>
2024-09-10 17:27:23 +0200

    Partie allocation

--- main.c
+++ main.c
@@ -4,31 +4,27 @@
 
 #include "functions.h"
 
+int variable;
+static int variable_static;
 
-int variable; // variable globale : segment de données aussi
-static int variable_static; // variable globale statique : segment de données
+#define MAX_SIZE 10
+int tabSize = 0;
 
-int tabSize; // variable du segment de données
+char filename[] = "num.txt";
 
+int main(void) {
+    FILE *fptr = fopen(filename, "r");
 
-int main (void) {
-
-    printf("Enter Tab size: \n");
-    scanf("%d", &tabSize);
 
     srand(time(NULL));
 
-    int tab[tabSize]; // dans la pile
+    int tab[MAX_SIZE];
+    int num;
 
-    printf("Random list: \n");
-    for (int i=0;i<tabSize; i++) {
-        tab[i] = (rand() % 99) + 1;
-        printf("%i \n", tab[i]);
+    while (fscanf(fptr, "%d", &num) == 1 && tabSize < MAX_SIZE) {
+        tab[tabSize++] = num;
     }
-    printf("The max value is %i (index: %i) and the min value is %i (index: %i) \n", getMaxValue(tab), getIndex(getMaxValue(tab), tab), getMinValue(tab), getIndex(getMinValue(tab), tab));
-    printf("Sorted Tab: \n");
-    sortTab(tab);
-    return 0;
+
+    fclose(fptr);
+
 }
-
-
